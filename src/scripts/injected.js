2073c1e7d1c2fdd557e845e6a3ca75282b060f2b
Tymoteusz Bartnik <battnik90@gmail.com>
2020-11-30 13:00:38 +0100

    Add fetch api

--- src/scripts/injected.js
+++ src/scripts/injected.js
@@ -24,12 +24,13 @@
         modal = document.createElement('div');
 
         let modalHtml = '<div id="link-collector-modal-content">';
-        modalHtml += '<div id="link-collector-modal-loader-wrapper"><div id="link-collector-modal-loader"></div></div>';
+        modalHtml += '<div class="link-collector-modal-full-wrapper" id="link-collector-modal-loader-wrapper"><div id="link-collector-modal-loader"></div></div>';
+        modalHtml += '<div class="link-collector-modal-full-wrapper" id="link-collector-modal-result-wrapper"><div id="link-collector-modal-result"></div></div>';
         modalHtml += '<div id="link-collector-modal-header"><h1>KNI Link Collector</h1><span id="link-collector-modal-close">&times;</span></div>';
         modalHtml += '<div id="link-collector-modal-body"><form id="link-collector-modal-form">';
-        modalHtml += '<div class="link-collector-modal-form-group"><label>Link</label><input name="link-collector-link" placeholder="https://kni.mini.pw.edu.pl" value="' + window.location.href +'"></div>';
-        modalHtml += '<div class="link-collector-modal-form-group"><label>Link description</label><input name="link-collector-description" placeholder="cebula"></div>';
-        modalHtml += '<button id="link-collector-modal-button">Save</button>';
+        modalHtml += '<div class="link-collector-modal-form-group"><label>Link</label><input id="link-collector-link" name="link-collector-link" placeholder="https://kni.mini.pw.edu.pl" value="' + window.location.href +'" required></div>';
+        modalHtml += '<div class="link-collector-modal-form-group"><label>Link description</label><input id="link-collector-description" name="link-collector-description" placeholder="Very interesting link" required></div>';
+        modalHtml += '<div class="link-collector-modal-button-group"><button id="link-collector-modal-button">Save</button></div>';
         modalHtml += '</form></div>';
         modalHtml += '</div>';
         modal.innerHTML = modalHtml;
@@ -77,8 +78,46 @@
             removeModal();
     }
 
-    function formSubmit(e) {
-        e.preventDefault();
-        document.getElementById('link-collector-modal-loader-wrapper').style.display = 'flex';
+    async function formSubmit(e) {
+        let result;
+
+        try {
+            e.preventDefault();
+            document.getElementById('link-collector-modal-loader-wrapper').style.display = 'flex';
+
+            let data = {
+                link: document.getElementById('link-collector-link').value,
+                link_description: document.getElementById('link-collector-description').value
+            }
+
+            result = await fetch("https://razormeister.pl/link_collector/", {
+                method: "post",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if(data.success)
+                        return "Success!";
+                    else {
+                        console.log("KNI LinkCollector error: " + data);
+                        return "Error: " + data.msg;
+                    }
+                }).catch((error) => {
+                    console.log("KNI LinkCollector error: " + error);
+                    return "Error: " + error.message;
+                });
+        } catch (error) {
+            console.log("KNI LinkCollector error: " + error);
+            result = "Error: " + error.message;
+        }
+
+        document.getElementById('link-collector-modal-loader-wrapper').style.display = 'none';
+        document.getElementById('link-collector-modal-result').innerHTML = result;
+        document.getElementById('link-collector-modal-result-wrapper').style.display = 'flex';
+
+        setTimeout(() => document.getElementById('link-collector-modal-result-wrapper').style.display = 'none', 2000);
     }
 })();
